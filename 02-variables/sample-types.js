aef998a3506151dfcd223670c33a3d653a402543
padamv <padamv91@gmail.com>
2021-01-04 13:13:52 +0100

    Use of types in typescript

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,9 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Anup";
+var lastName = "Kumar";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+// use template Strings
+console.log("Hi " + firstName + " " + lastName);
