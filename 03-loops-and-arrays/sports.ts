af51d52d500d9c4994f0036115ef92b00c85bf2f
padamv <padamv91@gmail.com>
2021-01-04 13:40:51 +0100

    simplified for and if-else

---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,11 @@
+let sportsOne: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
+
+for (let tempSport of sportsOne) {
+    if ( tempSport == "Cricket"){
+        console.log(tempSport + " << My favourite sport!");
+    }
+    else{
+        console.log(tempSport);
+    }
+    
+}
