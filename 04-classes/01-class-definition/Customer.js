d86458823ec0f6f2eb8cf9309b80866dca8b7e2f
padamv <padamv91@gmail.com>
2021-01-04 13:58:36 +0100

    Create class

---
+++ 04-classes/01-class-definition/Customer.js
@@ -0,0 +1,11 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+// let's create an instance
+var myCustomer = new Customer();
+myCustomer.firstName = "Martin";
+myCustomer.lastName = "Dixon";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ 04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,15 @@
+class Customer {
+
+    firstName: string;
+    lastName: string;
+
+}
+
+// let's create an instance
+let myCustomer = new Customer();
+
+myCustomer.firstName = "Martin";
+myCustomer.lastName = "Dixon";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
